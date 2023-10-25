8120f7355ed8e66c7bb674d4dc15643658a5fada
jeonghyun <invent819@naver.com>
2023-10-26 00:11:06 +0900

    color, text style setting

--- lib/common/common_style.dart
+++ lib/common/common_style.dart
@@ -32,28 +32,28 @@ const NONE_COLOR = Color(0xff810143);
   w900 Black, the most thick
  */
 
-TextStyle Heading_01 = TextStyle(fontFamily: 'IBMPlexSansKR',fontWeight: FontWeight.w700, fontSize: 38.sp);
-TextStyle Heading_02 = TextStyle(fontFamily: 'IBMPlexSansKR',fontWeight: FontWeight.w700, fontSize: 32.sp, height: 30.h);
-TextStyle Heading_03 = TextStyle(fontFamily: 'IBMPlexSansKR',fontWeight: FontWeight.w700, fontSize: 24.sp);
-TextStyle Heading_04 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 20.sp);
-TextStyle Heading_05 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 18.sp, height: 36.h);
-TextStyle Heading_06 = TextStyle(fontFamily: 'NotoSansKR', fontWeight: FontWeight.w500, fontSize: 16.sp, height: 30.h);
-TextStyle Heading_07 = TextStyle(fontFamily: 'NotoSansKR', fontWeight: FontWeight.w900, fontSize: 14.sp, height: 30.h);
+final TextStyle Heading_01 = TextStyle(fontFamily: 'IBMPlexSansKR',fontWeight: FontWeight.w700, fontSize: 38.sp);
+final TextStyle Heading_02 = TextStyle(fontFamily: 'IBMPlexSansKR',fontWeight: FontWeight.w700, fontSize: 32.sp, height: 30.h);
+final TextStyle Heading_03 = TextStyle(fontFamily: 'IBMPlexSansKR',fontWeight: FontWeight.w700, fontSize: 24.sp);
+final TextStyle Heading_04 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 20.sp);
+final TextStyle Heading_05 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 18.sp, height: 36.h);
+final TextStyle Heading_06 = TextStyle(fontFamily: 'NotoSansKR', fontWeight: FontWeight.w500, fontSize: 16.sp, height: 30.h);
+final TextStyle Heading_07 = TextStyle(fontFamily: 'NotoSansKR', fontWeight: FontWeight.w900, fontSize: 14.sp, height: 30.h);
 
-TextStyle m_Heading_01 = TextStyle(fontFamily: 'NotoSansKR', fontWeight: FontWeight.w700, fontSize: 14.sp);
-TextStyle m_Heading_02 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w700, fontSize: 12.sp);
-TextStyle m_Heading_03 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 10.sp);
-TextStyle m_Heading_04 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w700, fontSize: 7.sp, height: 18.h);
-TextStyle m_Heading_05 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w700, fontSize: 16.sp, height: 20.h);
+final TextStyle m_Heading_01 = TextStyle(fontFamily: 'NotoSansKR', fontWeight: FontWeight.w700, fontSize: 14.sp);
+final TextStyle m_Heading_02 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w700, fontSize: 12.sp);
+final TextStyle m_Heading_03 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 10.sp);
+final TextStyle m_Heading_04 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w700, fontSize: 7.sp, height: 18.h);
+final TextStyle m_Heading_05 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w700, fontSize: 16.sp, height: 20.h);
 
-TextStyle Button_01 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 20.sp);
-TextStyle Button_02 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 14.sp);
-TextStyle Button_03 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w500, fontSize: 12.sp);
+final TextStyle Button_01 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 20.sp);
+final TextStyle Button_02 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 14.sp);
+final TextStyle Button_03 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w500, fontSize: 12.sp);
 
-TextStyle m_Button_00 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w700, fontSize: 10.sp);
-TextStyle m_Button_01 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 8.sp);
-TextStyle m_Button_02 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 5.sp);
-TextStyle m_Button_03 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 6.sp, height: 12.h);
+final TextStyle m_Button_00 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w700, fontSize: 10.sp);
+final TextStyle m_Button_01 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 8.sp);
+final TextStyle m_Button_02 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 5.sp);
+final TextStyle m_Button_03 = TextStyle(fontFamily: 'IBMPlexSansKR', fontWeight: FontWeight.w600, fontSize: 6.sp, height: 12.h);
 
 
 TextStyle Body_01 = TextStyle(fontFamily: 'NotoSansKR', fontWeight: FontWeight.w500, fontSize: 14.sp, height: 30.h);
